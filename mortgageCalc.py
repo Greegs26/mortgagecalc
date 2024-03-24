27d0051d3278b4bcbc0c19693de96685bc3f75af
Greegs26 <greganchristian@gmail.com>
2024-03-24 20:35:47 -0300

    testing

--- mortgageCalc.py
+++ mortgageCalc.py
@@ -1,2 +1,12 @@
+# trying to use excel less
+# trying to test something
 
+mortgagecap=0.39
+debtcap=0.44
+
+user_salary=int(input("Please enter your yearly salary: "))
+print(user_salary)
+user_debtpayments=int(input("Please enter your total monthly debt payments: "))
+print(user_debtpayments)
+print(user_salary + user_debtpayments)
 
